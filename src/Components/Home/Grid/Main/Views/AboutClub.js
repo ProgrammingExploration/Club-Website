74d48ffe4554eeefd29172e4bf183c259bd1f8b7
Om Shivam Panda <sopanda0910@gmail.com>
2020-08-22 13:49:57 -0400

    Finished no responsive home page done

--- src/Components/Home/Grid/Main/Views/AboutClub.js
+++ src/Components/Home/Grid/Main/Views/AboutClub.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const AboutClub = () => {
     return (
-        <div>
-            AboutClub
+        <div className="aboutclub">
+            <h1>About Club</h1>
+            <p>
+                This club, you may have noticed if you looked through the courses, does cover tons of concepts and information. For example, if you look at the Web Development Course you learn HTML, CSS, Vanilla JavaScript, React JS, Node JS Express NEDB, React Native, TypeScript, and a bit about Angular. And, that is a lot of concepts to learn for one year, but you aren’t going to come out of this club having mastery knowledge on all of these topics, but you will have enough knowledge to make any app, website, game, or automation you want just by a little research. And you might be thinking that isn’t that possible even if I didn’t participate in the club, and my answer would be known. I believe in this theory that I call Barrier to Entry, anything you are trying to learn, you must have the basic knowledge and almost an intermediate level understanding, before you go on your own if you want to learn in the most efficient way possible. And I follow these rules as well. I go through and I find tutorials for all these different concepts while still being a beginner, and after finishing the course, I try building a project with this skill that I just learned, to fine tune my skills and also just to test myself. And, this club takes a perfect tour of all of these topics because in the beginning you learn and cross that barrier to entry, and once you do that, in the end of the club, you get sometime to research and create your own project, and you can enter the project into a competition. Hopefully, by crossing the barrier of entry for this club, you chose to participate in the Programming Exploration.
+             </p>
         </div>
     );
 }
--- src/Components/Home/Grid/Main/Views/Tools.js
+++ src/Components/Home/Grid/Main/Views/Tools.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 const Tools = () => {
     return (
-        <div>
-            Tools
+        <div className="toolinfo">
+            <h1>Tools</h1>
+            <p>
+                At its heart, Visual Studio Code features a lightning fast source code editor, perfect for day-to-day use. With support for hundreds of languages, VS Code helps you be instantly productive with syntax highlighting, bracket-matching, auto-indentation, box-selection, snippets, and more. Intuitive keyboard shortcuts, easy customization and community-contributed keyboard shortcut mappings let you navigate your code with ease. For serious coding, you'll often benefit from tools with more code understanding than just blocks of text. Visual Studio Code includes built-in support for IntelliSense code completion, rich semantic code understanding and navigation, and code refactoring.And when the coding gets tough, the tough get debugging. Debugging is often the one feature that developers miss most in a leaner coding experience, so we made it happen. Visual Studio Code includes an interactive debugger, so you can step through source code, inspect variables, view call stacks, and execute commands in the console. VS Code also integrates with build and scripting tools to perform common tasks making everyday workflows faster. VS Code has support for Git so you can work with source control without leaving the editor including viewing pending changes diffs.
+            </p><br></br>
+            <p>
+                VS Code or Visual Studio Code is a powerful code editor, and the best thing about this tool is that it is completely free. This is a code editor/IDE which is amazing, and several people use it, including me, while building huge projects, building startups, and even employees in google use this product. The reason that so many programmers and developers use this code editor is because you can completely customize it to your needs. Starting from changing the theme color of the editor, to creating shortcuts that paste in boiler plate code for you, VS Code is the most customizable editor out there. We will use this editor in ⅘ of the code editors that we use, and the exception is App Development because there is an even more powerful tool supporting just app development, Android Studio. 
+            </p>
         </div>
     );
-}
+} 
 
 
 export default Tools;
